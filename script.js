2edda7505b615533d5bd1f935b8340332e3cfb5f
Piyush Kumar <138502686+Piykr20@users.noreply.github.com>
2024-05-18 13:49:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var typed = new Typed('#element', {
+    strings: ['web developer.', 'UI/UX Designer.', 'Video Editor.'],
+    typeSpeed: 100,
+    loop: true
+  });
+  
+  
+  // const toTop = document.querySelector(".top");
+  // window.addEventListener("scroll", () => {
+  //   if (window.pageYOffset > 100) {
+  //     toTop.classList.add("active");
+  //   }
+  //   else {
+  //     toTop.classList.remove("active");
+  //   }
+  // })
